ca6c89bc509c6a845cbd981a8496b6b5d966faa4
grueld <tonton_38@hotmail.fr>
2012-11-30 18:48:14 -0500

    premiere mise en plit

--- src/Window.java
+++ src/Window.java
@@ -13,8 +13,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
-public 	class Window extends JFrame implements ActionListener, ComponentListener {
+public class Window extends JFrame implements ActionListener, ComponentListener {
 	private static final long serialVersionUID = 1L;
 	private final double RATIO = 16 / (double) 9;
 
@@ -25,7 +24,7 @@ public 	class Window extends JFrame implements ActionListener, ComponentListener
 	private JTextField text;
 	private JLabel label1;
 	private JLabel label2;
-	
+
 	private JButton button1;
 	private JButton button2;
 	private JButton button3;
@@ -33,35 +32,36 @@ public 	class Window extends JFrame implements ActionListener, ComponentListener
 	private JButton button5;
 	private JButton button6;
 	private JButton button7;
-	
-	private JOptionPane jop = new JOptionPane() ;
+
+	private JOptionPane jop = new JOptionPane();
 
 	public Window() {
 		// window settings
-		setTitle("allo") ;
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		setLocation (100,100) ;
-		setResizable(true) ;
-		setVisible(true) ;
+		setTitle("allo");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocation(100, 100);
+		setResizable(true);
+		setSize(270, 480);
+		setVisible(true);
 
 		contentPane = new JPanel();
-		contentPane.setLayout(new LayoutMan()) ;
-		
-		button1 = new JButton("Ass") ;
-		button1.addActionListener(this) ;
-		
+		contentPane.setLayout(new LayoutMan());
+
+		button1 = new JButton("Ass");
+		button1.addActionListener(this);
+
 		label1 = new JLabel("LABEL1");
 		button2 = new JButton("BUTTON2");
 		text = new JTextField("TEXTFIELD");
 		button3 = new JButton("BUTTON3");
-		String[] listContents = {"Item1", "Item2"};
+		String[] listContents = { "Item1", "Item2" };
 		list = new JList(listContents);
 		button4 = new JButton("BUTTON4");
 		button5 = new JButton("BUTTON5");
 		button6 = new JButton("BUTTON6");
 		button7 = new JButton("BUTTON7");
 		label2 = new JLabel("LABEL2");
-		
+
 		contentPane.add(label1);
 		contentPane.add(button1);
 		contentPane.add(button2);
@@ -73,7 +73,7 @@ public 	class Window extends JFrame implements ActionListener, ComponentListener
 		contentPane.add(button6);
 		contentPane.add(button7);
 		contentPane.add(label2);
-		contentPane.addComponentListener(this);
+//		contentPane.addComponentListener(this);
 		setContentPane(contentPane);
 		pack();
 	}
@@ -84,41 +84,41 @@ public 	class Window extends JFrame implements ActionListener, ComponentListener
 		if (arg0.getSource() == button1) {
 			jop.showMessageDialog(getComponent(0), "HOLE");
 		}
-		
+
 	}
-	
+
 	public void componentResized(ComponentEvent e) {
-		Container window = e.getComponent().getParent().getParent().getParent();
-		//System.out.println(window.getClass());
-		Dimension compDim = window.getSize();
-		double x = compDim.width;  
-	    double y = compDim.height;  
-	    if (x / y < RATIO) {  
-	      compDim.width = (int) (y * RATIO);  
-	      compDim.height = (int) y;  
-	    } else {  
-	    	compDim.width = (int) x;  
-	    	compDim.height = (int) (x / RATIO);  
-	    }  
-	    window.setSize(compDim);
-	    System.out.println(compDim);
+//		Container window = e.getComponent().getParent().getParent().getParent();
+//		// System.out.println(window.getClass());
+//		Dimension compDim = window.getSize();
+//		double x = compDim.width;
+//		double y = compDim.height;
+//		if (x / y < RATIO) {
+//			compDim.width = (int) (y * RATIO);
+//			compDim.height = (int) y;
+//		} else {
+//			compDim.width = (int) x;
+//			compDim.height = (int) (x / RATIO);
+//		}
+//		window.setSize(compDim);
+//		System.out.println(compDim);
 	}
 
 	@Override
 	public void componentHidden(ComponentEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void componentMoved(ComponentEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void componentShown(ComponentEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
